954ae7640ac3c2157486f7c481387172148c2e8f
Tristan Ackermann <hallo@tristan-ackermann.de>
2024-02-09 23:03:12 +0100

    build: add scss

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
+import './scss/app.scss' // Add this line
+
 import HelloWorldScene from './HelloWorldScene'
 import Phaser from 'phaser'
 
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
 	parent: 'app',
-	width: 800,
-	height: 600,
+	width: 900,
+	height: 1600,
 	physics: {
 		default: 'arcade',
 		arcade: {
