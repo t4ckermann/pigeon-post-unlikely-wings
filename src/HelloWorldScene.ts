0459d0b634ae046be22f5ac51e14abb0ad76d682
Tristan Ackermann <hallo@tristan-ackermann.de>
2024-02-10 00:32:15 +0100

    feat: add experimental touch control

--- src/HelloWorldScene.ts
+++ src/HelloWorldScene.ts
@@ -14,12 +14,51 @@ export default class HelloWorldScene extends Phaser.Scene {
     }
 
     create() {
-        this.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'sky')
+        this.input.enabled = true;
+
+        let touchStartX = 0;
+        let touchEndX = 0;
+
+        initialiseListeners(this.input);
+        drawBackground(this);
 
         const logo = this.physics.add.image(this.cameras.main.centerX, this.cameras.main.centerY, 'logo')
-
         logo.setVelocity(100, 200)
         logo.setBounce(1, 1)
         logo.setCollideWorldBounds(true)
+
+        function drawBackground(scene: Phaser.Scene) {
+            scene.add.image(scene.cameras.main.centerX, scene.cameras.main.centerY, 'sky')
+        }
+
+        function handleSwipe() {
+            const SWIPE_DISTANCE = 10; // Minimum distance for a swipe
+            if (touchEndX - touchStartX > SWIPE_DISTANCE) {
+                // Swipe right
+                logo.setVelocity(200, 0);
+            } else if (touchStartX - touchEndX > SWIPE_DISTANCE) {
+                // Swipe left
+                logo.setVelocity(-200, 0);
+            }
+        }
+
+        function initialiseListeners(input: Phaser.Input.InputPlugin) {
+            // Listen for swipe input
+            input.on('pointerdown', function (pointer: PointerEvent) {
+                touchStartX = pointer.x;
+            });
+            input.on('touchstart', function (pointer: PointerEvent) {
+                touchStartX = pointer.x;
+            });
+
+            input.on('pointerup', function (pointer: PointerEvent) {
+                touchEndX = pointer.x;
+                handleSwipe();
+            });
+            input.on('touchend', function (pointer: PointerEvent) {
+                touchEndX = pointer.x;
+                handleSwipe();
+            });
+        }
     }
 }
