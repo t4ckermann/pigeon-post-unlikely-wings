6d3a15a4fcdac9f9103de9c88295054c0201a70d
Tristan Ackermann <hello@tristan-ackermann.de>
2024-02-24 16:22:22 +0100

    tidy: remove unused files

--- src/types/ExtendedPhaserGame.ts
+++
@@ -1,6 +0,0 @@
-import { Game } from 'phaser';
-import { LocalStorage } from '../utils/localStorage';
-
-export class ExtendedPhaserGame extends Game {
-  globalStorage: LocalStorage = new LocalStorage();
-}
